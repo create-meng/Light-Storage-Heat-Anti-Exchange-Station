31b9b13389175defc4dc169a72a222bad2b54cd5
creare-meng <1375528118@qq.com>
2026-03-09 01:39:27 +0800

    ui修复

---
+++ demo_web/.next-build/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"2upDl4BQVkVGLhOJ7JTYwOzk5hSPQjo8OmWNNdUO/tg=\"\n}"
--- demo_web/next.config.js
+++ demo_web/next.config.js
@@ -3,10 +3,9 @@ const isProd = process.env.NODE_ENV === 'production'
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
 
 const nextConfig = {
-  // dev 和 build 使用不同目录，避免冲突
-  distDir: isProd ? '.next-build' : '.next',
-  ...(isProd ? { output: 'export' } : {}),
-  ...(isProd && basePath
+  distDir: '.next-build',
+  output: 'export',
+  ...(basePath
     ? {
         basePath,
         assetPrefix: `${basePath}/`,
